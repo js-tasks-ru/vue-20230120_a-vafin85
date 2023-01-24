5f15b2063acb1233008fdf82c82d69f9a2b876ed
Vafin Artem <a.vafin85@gmail.com>
2023-01-25 00:47:23 +0500

    Выполняет задание VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: `App`,
+  data() {
+    return {
+      value1: 0,
+      value2: 0,
+      operand: null,
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.operand) {
+        case `sum`:
+          return this.value1 + this.value2;
+        case `subtract`:
+          return this.value1 - this.value2;
+        case `multiply`:
+          return this.value1 * this.value2;
+        case `divide`:
+          return this.value1 / this.value2;
+        default:
+          return 0;
+      }
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
