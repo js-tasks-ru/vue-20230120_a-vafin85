3cb5980a3df1e7e8db150b9f34cdf3bb54fc3596
Vafin Artem <a.vafin85@gmail.com>
2023-01-28 22:18:58 +0500

    Выполняет задание MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,12 +1,22 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  components: {MeetupAgendaItem},
+
+  props: {
+    agenda: {
+      type: Array,
+      require: true,
+    }
+  },
+
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
+      <li v-for="agendaItem in agenda" class="agenda__item">
+        <MeetupAgendaItem :agenda-item="agendaItem" />
         <!-- meetup agenda item -->
       </li>
     </ul>`,
