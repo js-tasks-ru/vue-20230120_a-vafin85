c58940c73556dfd14edc96e2f89b388403ed4761
Vafin Artem <a.vafin85@gmail.com>
2023-02-13 21:53:16 +0500

    Дорабатывает задание UiFormGroup1

--- 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
+++ 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :class="['form-group', {
+  <div class="form-group" :class="{
     'form-group_inline': inline
-  }]">
+  }">
     <label v-if="label" class="form-group__label">{{ label }}</label>
     <slot></slot>
   </div>
