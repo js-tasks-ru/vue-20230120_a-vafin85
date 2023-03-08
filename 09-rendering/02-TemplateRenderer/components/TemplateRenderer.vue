232474f429fd7e4d5614e4427bcd648545cbc8b0
Vafin Artem <a.vafin85@gmail.com>
2023-03-08 21:27:17 +0500

    Выполняет задание TemplateRenderer

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,24 +1,44 @@
-<script>
-// import { compile } from 'vue';
+<script lang="ts">
+import {compile, defineComponent, h} from 'vue';
+import type { PropType, DefineComponent, CreateComponentPublicInstance, VNode } from 'vue';
 
-export default {
+export default defineComponent({
   name: 'TemplateRenderer',
 
   props: {
     template: {
-      type: String,
+      type: String as PropType<string>,
       required: true,
     },
 
     bindings: {
-      type: Object,
+      type: Object as PropType<Record<string, string | unknown>>,
       default: () => ({}),
     },
 
     components: {
-      type: [Object, Array],
+      type: [Object, Array] as PropType<Record<string, CreateComponentPublicInstance>>,
       default: () => [],
     },
   },
-};
+
+  computed: {
+    renderFunction() {
+      return compile(this.template);
+    },
+
+    componentFromTemplate(): DefineComponent {
+      return defineComponent({
+        name: 'TemplateComponent',
+        components: this.components,
+        props: ['bindings'],
+        render: this.renderFunction,
+      });
+    },
+  },
+
+  render(): VNode {
+    return h(this.componentFromTemplate, { bindings: this.bindings });
+  },
+});
 </script>
